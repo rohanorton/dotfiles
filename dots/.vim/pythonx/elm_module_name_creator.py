6dafcfe5d8a6c754b5e1d2bfa89cbfeb956d5fb1
Rohan Orton <rohan.orton@gmail.com>
2019-01-19 16:13:50 +0000

    Fix crash on elm-19 projects (cannot find elm.json)

--- dots/.vim/pythonx/elm_module_name_creator.py
+++ dots/.vim/pythonx/elm_module_name_creator.py
@@ -6,21 +6,36 @@ import json
 import os.path
 import re
 
-def get_source_directory_paths(root_path):
-    filename = os.path.join(root_path, 'elm-package.json')
+def file_exists(path, filename):
+    file_path = os.path.join(path, filename)
+    return os.path.isfile(file_path)
+
+def get_package_file(path):
+    if file_exists(path, 'elm.json'):
+        return os.path.join(path, 'elm.json')
+    if file_exists(path, 'elm-package.json'):
+        return os.path.join(path, 'elm-package.json')
+    raise FileNotFoundError('Could not find elm.json or elm-package.json files.')
+
+def get_source_directory_paths(project_root):
+    filename = get_package_file(project_root)
     with open(filename) as elm_package_json:
         data = json.load(elm_package_json)
         return data['source-directories']
 
-def find_root_directory(module_path):
-    current_path = module_path
-    while (current_path != '/'):
-        elm_package_json = os.path.join(current_path, 'elm-package.json')
-        if os.path.isfile(elm_package_json):
-            return current_path
+def has_package_file(path):
+    return file_exists(path, 'elm.json') or file_exists(path, 'elm-package.json')
+
+def walk_dir_until(start, test):
+    current = start
+    while (current != '/'):
+        if test(current):
+            return current
         else:
-            current_path = os.path.dirname(current_path)
-    return '.'
+            current = os.path.dirname(current)
+
+def find_project_root(module_path):
+    return walk_dir_until(module_path, has_package_file)
 
 def remove_elm_extension(file_path):
     if file_path.endswith('.elm'):
@@ -43,18 +58,23 @@ def strip_source_directory(module_path, root_path, source_dirs):
     # If this fails, just return the filename.
     return os.path.basename(module_path)
 
+def build_orphan_module_name(module_path):
+    filename = os.path.basename(module_path)
+    return remove_elm_extension(filename)
+
+def build_project_module_name(project_root, module_path):
+    source_dirs = get_source_directory_paths(project_root)
+    module_path_from_source = strip_source_directory(module_path, project_root, source_dirs)
+    module_path_without_extension = remove_elm_extension(module_path_from_source)
+    return convert_path_to_dot_separated(module_path_without_extension)
 
 def convert_path_to_module_name(module_path):
-    root_path = find_root_directory(module_path)
+    project_root = find_project_root(module_path)
 
-    try:
-        source_dirs = get_source_directory_paths(root_path)
-    except:
-        source_dirs = []
+    # If not in a project, fallback to using filename.
+    if project_root == None:
+        return build_orphan_module_name(module_path)
 
-    module_path_from_source = strip_source_directory(module_path, root_path, source_dirs)
-    module_path_without_extension = remove_elm_extension(module_path_from_source)
-    module_name = convert_path_to_dot_separated(module_path_without_extension)
-    return module_name
+    return build_project_module_name(project_root, module_path)
 
 
